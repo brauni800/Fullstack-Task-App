9954b6036765905bc299119d21b1e540cd75a984
Javier González Juárez <cabojavo@hotmail.com>
2025-02-20 14:44:24 -0600

    🚀 Initial Backend Configuration: Set up core settings, environment variables, and project structure.

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routes import users, tasks
+
+app = FastAPI()
+
+# Incluir las rutas
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
+
+
+@app.get("/")
+def root():
+    return {"message": "Task Manager API using FastAPI"}
